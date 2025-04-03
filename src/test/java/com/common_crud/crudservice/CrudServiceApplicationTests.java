f6c7246d380c006182a11b3204907dfdb608c60b
raniduharshana <rharshana@lk.onsystechnologies.com>
2025-04-03 13:12:34 +0530

    initial commit

---
+++ src/test/java/com/common_crud/crudservice/CrudServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.common_crud.crudservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrudServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
